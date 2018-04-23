e7bfbf3c04dcc63d05959380fa32be46692c13eb
Kevin Newman <Kevin@unFocus.com>
2018-04-23 16:39:53 -0400

    add basic service worker

---
+++ client/service-worker.js
@@ -0,0 +1,9 @@
+// import { Meteor } from 'meteor/meteor'
+// Meteor.startup(async () => {
+//   if (!navigator.serviceWorker) return
+//   try {
+//     await navigator.serviceWorker.register('/sw.js')
+//   } catch (error) {
+//     console.error('ServiceWorker registration failed: ', error)
+//   }
+// })
