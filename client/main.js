8d849c5372a37134351bc412a92bdd100e3e439b
Kevin Newman <Kevin@unFocus.com>
2018-07-19 18:07:38 -0400

    integrate apollo SSR on client side (hydration) and other tweeks

--- client/main.js
+++ client/main.js
@@ -1,29 +1,33 @@
-import './apollo'
+import { client } from './apollo'
 import './service-worker'
 
 import { onPageLoad } from 'meteor/server-render'
 import React from 'react'
 import Loadable from 'react-loadable'
+import { ApolloProvider } from 'react-apollo'
 import { HelmetProvider } from 'react-helmet-async'
 import { hydrate } from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 
 h = React.createElement // eslint-disable-line
 
-onPageLoad(async sink => {
+onPageLoad(async () => {
   let App = (await import('/imports/App')).default
 
   if (window.__preloadables__) {
     await Loadable.preloadablesReady(window.__preloadables__)
+    // Clean up after Preloadables
+    const script = document.getElementById('__preloadables__')
+    script.parentNode.removeChild(script)
+    delete window.__preloadables__
   }
 
   const helmetContext = {}
-  hydrate(
+  hydrate(<ApolloProvider client={client}>
     <HelmetProvider context={helmetContext}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </HelmetProvider>,
-    document.getElementById('root')
-  )
+    </HelmetProvider>
+  </ApolloProvider>, document.getElementById('root'))
 })
