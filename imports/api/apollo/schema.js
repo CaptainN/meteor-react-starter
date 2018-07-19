4c628cdf016a974c028b7bcda8a08410f2c84436
Kevin Newman <Kevin@unFocus.com>
2018-07-19 17:59:48 -0400

    make executable schema for use on server

--- imports/api/apollo/schema.js
+++ imports/api/apollo/schema.js
@@ -1,7 +1,14 @@
 import gql from 'graphql-tag'
+import { makeExecutableSchema } from 'graphql-tools'
+import { resolvers } from './resolvers'
 
 export const typeDefs = gql`
 type Query {
   say: String!
 }
 `
+
+export const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
