79947ad48518e96cdf770b6dfba9990917fbb1a7
Kevin Newman <Kevin@unFocus.com>
2019-07-18 13:23:46 -0400

    Fix missing page data

--- imports/ui/pages/Page.js
+++ imports/ui/pages/Page.js
@@ -2,7 +2,7 @@ import { usePage } from '/imports/api/connectors/pages'
 
 const Page = ({ slug }) => {
   const [pages, isLoading] = usePage({ slug })
-  return <div dangerouslySetInnerHTML={{ __html: isLoading ? 'Loading' : pages[0].content }} />
+  return <div dangerouslySetInnerHTML={{ __html: isLoading || pages.length === 0 ? 'Loading' : pages[0].content }} />
 }
 
 export default Page
